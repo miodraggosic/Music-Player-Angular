932db485cdf70185b2069a2c7bfb52fcafddba64
miodraggosic <gosmio88@gmail.com>
2023-06-07 22:59:19 +0200

    update: added home routes

--- Music-Player/src/app/app-routing.module.ts
+++ Music-Player/src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeLayoutComponent } from './core/layouts/home-layout/home-layout.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./features/songs/songs.module').then((m) => m.SongsModule),
+      },
+      {
+        path: 'admin',
+        loadChildren: () =>
+          import('./features/admin/admin.module').then((m) => m.AdminModule),
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
