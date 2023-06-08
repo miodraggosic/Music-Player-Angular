693068107798903904ee5283cf38ade6fee52b8d
miodraggosic <gosmio88@gmail.com>
2023-06-08 22:39:02 +0200

    update: material modules added

--- Music-Player/src/app/core/core.module.ts
+++ Music-Player/src/app/core/core.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { HeaderComponent } from './components/header/header.component';
 import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
 import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
@@ -9,16 +12,16 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const COMPONENTS = [
-  HeaderComponent,
   HomeLayoutComponent,
   UserLayoutComponent,
+  HeaderComponent,
   FooterComponent,
   PageNotFoundComponent,
 ];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],
   exports: [...COMPONENTS],
 })
 export class CoreModule {}
