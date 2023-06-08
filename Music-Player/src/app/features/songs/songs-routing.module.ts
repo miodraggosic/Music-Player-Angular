57c6ee24d776c630eeb138a7126e66a44b420f05
miodraggosic <gosmio88@gmail.com>
2023-06-09 00:17:04 +0200

    update: route to Songs added

--- Music-Player/src/app/features/songs/songs-routing.module.ts
+++ Music-Player/src/app/features/songs/songs-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SongsComponent } from './container/songs/songs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SongsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SongsRoutingModule { }
+export class SongsRoutingModule {}
--- Music-Player/src/app/features/songs/songs.module.ts
+++ Music-Player/src/app/features/songs/songs.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SongsRoutingModule } from './songs-routing.module';
 
+import { SongsComponent } from './container/songs/songs.component';
+import { SongComponent } from './components/song/song.component';
+import { SongCardComponent } from './components/song-card/song-card.component';
+
+const COMPONENTS = [SongsComponent, SongCardComponent, SongComponent];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SongsRoutingModule
-  ]
+  declarations: [...COMPONENTS],
+  imports: [CommonModule, SongsRoutingModule],
+  exports: [...COMPONENTS],
 })
-export class SongsModule { }
+export class SongsModule {}
