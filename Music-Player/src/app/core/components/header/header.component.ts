b0532d35764a7e11352b7008d821de04095751c4
miodraggosic <gosmio88@gmail.com>
2023-06-08 22:35:21 +0200

    update: navigation.service added & navLinks

--- Music-Player/src/app/core/components/header/header.component.ts
+++ Music-Player/src/app/core/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationService } from '../../services/navigation.service';
+import { NavLink } from '@models/interfaces/nav-link.interface';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  navLinks: NavLink[] = [];
+  title: string = 'mikimusic';
 
-  constructor() { }
+  constructor(private navigationService: NavigationService) {}
 
   ngOnInit(): void {
+    this.getNavLinks();
   }
 
+  private getNavLinks(): void {
+    this.navigationService
+      .getNavLinks()
+      .pipe(take(1))
+      .subscribe((data: NavLink[]) => (this.navLinks = data));
+  }
 }
