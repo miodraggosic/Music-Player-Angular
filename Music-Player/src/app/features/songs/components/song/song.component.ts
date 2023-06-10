216f04ad710d6e7d01c9ba3ebc29243a9b5c4aba
miodraggosic <gosmio88@gmail.com>
2023-06-11 00:35:38 +0200

    update: song input and template added

--- Music-Player/src/app/features/songs/components/song/song.component.ts
+++ Music-Player/src/app/features/songs/components/song/song.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Song } from '@models/interfaces/song.interface';
 
 @Component({
   selector: 'app-song',
   templateUrl: './song.component.html',
-  styleUrls: ['./song.component.scss']
+  styleUrls: ['./song.component.scss'],
 })
 export class SongComponent implements OnInit {
+  @Input() song!: Song | null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
