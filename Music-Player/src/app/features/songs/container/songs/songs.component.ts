664560bb7efc8b9ad9965d4a5e2e41e66f94c3e7
miodraggosic <gosmio88@gmail.com>
2023-06-11 00:32:59 +0200

    update: search logic implement

--- Music-Player/src/app/features/songs/container/songs/songs.component.ts
+++ Music-Player/src/app/features/songs/container/songs/songs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Song } from '@models/interfaces/song.interface';
 import { SongsService } from '../../services/songs.service';
 import { take } from 'rxjs';
@@ -9,6 +9,7 @@ import { take } from 'rxjs';
   styleUrls: ['./songs.component.scss'],
 })
 export class SongsComponent implements OnInit {
+  @Input() searchTerm: string = '';
   songs: Song[] = [];
 
   constructor(private songsService: SongsService) {}
@@ -17,10 +18,26 @@ export class SongsComponent implements OnInit {
     this.getAllSongs();
   }
 
+  onSearchTerm(event: string) {
+    this.searchTerm = event;
+    this.getFiltered();
+  }
+
   private getAllSongs(): void {
     this.songsService
       .getAllSongs()
       .pipe(take(1))
       .subscribe((data) => (this.songs = data));
+  }
+
+  private getFiltered() {
+    this.songsService
+      .getFiltered(this.searchTerm)
+      .pipe(take(1))
+      .subscribe((data) => {
+        console.log(data);
+
+        this.songs = data;
+      });
   }
 }
