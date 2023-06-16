ddb55c26668a4fe97be0ae06e08f4064e84a75f9
miodraggosic <gosmio88@gmail.com>
2023-06-16 02:11:06 +0200

    update: routes added

--- Music-Player/src/app/features/admin/admin-routing.module.ts
+++ Music-Player/src/app/features/admin/admin-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminSongsComponent } from './container/admin-songs/admin-songs.component';
+import { AddSongComponent } from './components/add-song/add-song.component';
+import { EditSongComponent } from './components/edit-song/edit-song.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminSongsComponent,
+  },
+  {
+    path: 'addSong',
+    component: AddSongComponent,
+  },
+  {
+    path: 'editSong/:id',
+    component: EditSongComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
