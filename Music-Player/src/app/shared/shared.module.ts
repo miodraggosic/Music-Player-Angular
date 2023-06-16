8987b7ce7f85507e7850d64eef78c0af130de0f8
miodraggosic <gosmio88@gmail.com>
2023-06-16 20:51:37 +0200

    update: component declarations

--- Music-Player/src/app/shared/shared.module.ts
+++ Music-Player/src/app/shared/shared.module.ts
@@ -8,8 +8,9 @@ import { MatInputModule } from '@angular/material/input';
 import { SearchComponent } from './components/search/search.component';
 
 import { FullNamePipe } from './pipes/full-name.pipe';
+import { TextLengthPipe } from './pipes/text-length.pipe';
 
-const COMPONENTS = [SearchComponent, FullNamePipe];
+const COMPONENTS = [SearchComponent, FullNamePipe, TextLengthPipe];
 
 @NgModule({
   declarations: [...COMPONENTS],
