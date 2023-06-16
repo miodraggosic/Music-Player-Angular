2a5d90755b230e57d5f7d1415e040ba98e7e29df
miodraggosic <gosmio88@gmail.com>
2023-06-16 02:10:21 +0200

    update: component declarations

--- Music-Player/src/app/features/admin/admin.module.ts
+++ Music-Player/src/app/features/admin/admin.module.ts
@@ -1,14 +1,41 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from '@shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AddSongComponent } from './components/add-song/add-song.component';
+import { TableComponent } from './components/table/table.component';
+import { AdminSongsComponent } from './container/admin-songs/admin-songs.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { SongFormComponent } from './components/song-form/song-form.component';
+import { EditSongComponent } from './components/edit-song/edit-song.component';
+
+const COMPONENTS = [
+  AdminSongsComponent,
+  TableComponent,
+  AddSongComponent,
+  SongFormComponent,
+  EditSongComponent,
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [...COMPONENTS],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    MatTableModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
+  exports: [...COMPONENTS],
 })
-export class AdminModule { }
+export class AdminModule {}
