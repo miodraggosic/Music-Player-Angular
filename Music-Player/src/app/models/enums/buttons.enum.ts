15b2f76caf0516e5309b7cf316cadd4775d80e88
miodraggosic <gosmio88@gmail.com>
2023-06-18 13:45:57 +0200

    create: Titles, Buttons enums

---
+++ Music-Player/src/app/models/enums/buttons.enum.ts
@@ -0,0 +1,6 @@
+export enum Buttons {
+  LOGIN = 'login',
+  SIGNUP = 'SignUp',
+  ADD = 'Add',
+  UPDATE = 'Update',
+}
---
+++ Music-Player/src/app/models/enums/titles.enum.ts
@@ -0,0 +1,5 @@
+export enum Titles {
+  LIBRARY = 'Library',
+  NEWSONG = 'New Song',
+  EDITSONG = 'Edit Song',
+}
