b583d2fafadfd50a2c0b0d0ce58584e59897cd74
miodraggosic <gosmio88@gmail.com>
2023-06-18 21:50:25 +0200

    update:  navLinks service and auth methods added

--- Music-Player/src/app/core/components/header/header.component.ts
+++ Music-Player/src/app/core/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationService } from '../../services/navigation.service';
 import { NavLink } from '@models/interfaces/nav-link.interface';
 import { take } from 'rxjs';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,19 +10,41 @@ import { take } from 'rxjs';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  navLinks: NavLink[] = [];
   title: string = 'mikimusic';
+  navLinks!: NavLink[];
 
-  constructor(private navigationService: NavigationService) {}
+  admin: boolean = false;
+  logged: boolean = false;
+
+  constructor(
+    private navigationService: NavigationService,
+    private auth: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.getNavLinks();
+  }
+
+  logout() {
+    this.auth.logout();
+    this.isLogged();
+  }
+
+  private isAdmin(): void {
+    this.admin = this.auth.isAdmin();
+  }
+  private isLogged() {
+    this.isAdmin();
+    this.logged = !this.auth.isAuthenticated();
   }
 
   private getNavLinks(): void {
     this.navigationService
       .getNavLinks()
       .pipe(take(1))
-      .subscribe((data: NavLink[]) => (this.navLinks = data));
+      .subscribe((data: NavLink[]) => {
+        this.navLinks = data;
+        this.isLogged();
+      });
   }
 }
