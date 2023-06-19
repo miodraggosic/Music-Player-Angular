d815bfd59aed15b153be25da3d32f99529bfaff2
miodraggosic <gosmio88@gmail.com>
2023-06-19 07:51:45 +0200

    update:  Button, Title components added

--- Music-Player/src/app/features/admin/container/admin-songs/admin-songs.component.ts
+++ Music-Player/src/app/features/admin/container/admin-songs/admin-songs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Buttons } from '@models/enums/buttons.enum';
+import { Titles } from '@models/enums/titles.enum';
 import { Song } from '@models/interfaces/song.interface';
 import { take } from 'rxjs';
 import { SongsService } from 'src/app/features/songs/services/songs.service';
@@ -10,6 +12,8 @@ import { SongsService } from 'src/app/features/songs/services/songs.service';
 })
 export class AdminSongsComponent implements OnInit {
   songs: Song[] = [];
+  buttonText: string = Buttons.NEWSONG;
+  titleText: string = Titles.LIBRARY;
 
   constructor(private songsService: SongsService) {}
 
